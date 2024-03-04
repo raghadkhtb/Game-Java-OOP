2b84d313ae33b040e0f7a8f357ea7e85cc958ddd
Raghad <raghadkhatib04@gmail.com>
2024-03-04 11:51:20 +0330

    first class is created

---
+++ src/game.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author acer
+ *
+ */
+public class game {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
