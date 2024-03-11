c4a32663376e6e220e1bae6d844ed95c411d80ae
Raghad <raghadkhatib04@gmail.com>
2024-03-11 12:08:47 +0330

    Player2 is done

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,20 @@
 
 public class Main {
-
+	
+	public static void main (String[]args) {
+		Player1 hussien = new Player1 ("Hussien" , "AK23" , 100);
+		//System.out.println(hussien.getName());
+		//System.out.println(hussien.getHealth());
+		//System.out.println(hussien.getWeapon());
+       hussien.damageByGun1();
+       hussien.damageByGun2();
+       hussien.healPro();
+       //hussien.damageByGun2();
+		Player2 proHussien = new Player2 ("ProHussien" , "ShotGun" , 80 , true);
+		
+        //proHussien.damageByGun1();
+       // proHussien.heal();
+       //proHussien.damageByGun2();
+		
+}
 }
--- src/Player2.java
+++ src/Player2.java
@@ -1,4 +1,52 @@
 
-public class Player2 {
+public class Player2 extends Player1 {
+private int health;
+private boolean shield;
+public Player2(String name, String weapon, int health , boolean shield) {
+	super(name,weapon,health);
+	this.health = health;
+	this.shield = shield ;
+	
+}
+@Override
+public void damageByGun1() {
+	if (shield) {
+		this.health -= 10;
+		if (this.health <= 0 ) this.health = 0;
+		System.out.println("Shield is ON! Got hit by Gun1. Health is reduced by 10. New health is " + this.health);
+	}
+		if (!shield) {
+			this.health -= 40;
+			if (this.health <= 0 ) this.health = 0;
+			System.out.println("SHield is OFF! Got hit by Gun1. Health is reduced by 40. New health is " + this.health);
+	}
+}
+@Override
+public void damageByGun2() {
+	if (shield) {
+		this.health -= 20;
+		if (this.health <= 0 ) this.health = 0;
+		System.out.println("SHield is ON! Got hit by Gun2. Health is reduced by 20. New health is " + this.health);
+	}
+	if (!shield) {
+		this.health -= 70;
+		if (this.health <= 0 ) this.health = 0;
+		System.out.println("SHield is OFF! Got hit by Gun2. Health is reduced by 70. New health is " + this.health);
+	}
 
 }
+@Override
+public void heal() {
+	// TODO Auto-generated method stub
+	super.heal();
+}
+@Override
+public void healPro() {
+	// TODO Auto-generated method stub
+	super.healPro();
+}
+
+	
+}
+
+
